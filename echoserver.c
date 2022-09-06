f318a36988d336d793bdce0bd6df5523b3e960d2
gaosong628 <51213408+gaosong628@users.noreply.github.com>
2022-09-06 14:55:27 -0500

    Update echoserver.c

--- echoserver.c
+++ echoserver.c
@@ -85,7 +85,7 @@ int main(int argc, char **argv) {
   memset(&server_addr, '\0', sizeof(server_addr));
   server_addr.sin_family = AF_INET;
   server_addr.sin_port = portno;
-  server_addr.sin_addr.s_addr = inet_addr(ip);
+  server_addr.sin_addr.s_addr = INADDR_ANY;
 
   n = bind(server_sock, (struct sockaddr*)&server_addr, sizeof(server_addr));
   if (n < 0){
