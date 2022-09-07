68460ba1955697dececf43d3f96179ff40417478
gaosong628 <51213408+gaosong628@users.noreply.github.com>
2022-09-06 20:31:22 -0500

    Update echoserver.c

--- echoserver.c
+++ echoserver.c
@@ -70,7 +70,7 @@ int main(int argc, char **argv) {
   }
 
   /* Socket Code Here */
-
+  /*create a socket and verify*/
   server_sock = socket(AF_INET, SOCK_STREAM, 0);
   if (server_sock < 0){
     perror("[-]Socket error");
@@ -78,34 +78,39 @@ int main(int argc, char **argv) {
   }
   printf("[+]TCP server socket created.\n");
 
-  memset(&server_addr, '\0', sizeof(server_addr));
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_port = portno;
-  server_addr.sin_addr.s_addr = INADDR_ANY;
-
+  memset(&server_addr, '\0', sizeof(server_addr)); /*set server_addr to null*/
+  server_addr.sin_family = AF_INET; /*family type*/
+  server_addr.sin_port = portno; /*port number*/
+  server_addr.sin_addr.s_addr = INADDR_ANY; /*internet address*/
+  /*bind newly created socket to a given IP address and verify it*/
   n = bind(server_sock, (struct sockaddr*)&server_addr, sizeof(server_addr));
   if (n < 0){
     perror("[-]Bind error");
     exit(1);
   }
   printf("[+]Bind to the port number: %d\n", portno);
-
+  
+  /*make sure server is ready to listen*/
   listen(server_sock, 5);
   printf("Listening...\n");
+  
   while(1)
   {
+      /*accept data package*/
       addr_size=sizeof(client_addr);
       client_sock=accept(server_sock, (struct sockaddr*)&client_addr, &addr_size);
       printf("[+]Client connected.\n");
       
     while(1) {
       bzero(buffer,BUFSIZE);
+      /*accept data and save it into buffer*/
       recv(client_sock, buffer, sizeof(buffer),0);
       
       printf("Message Received from Client: %s\n",buffer);
+      /*send the same message back to the client*/
       send(client_sock,buffer,strlen(buffer),0);
     }
-      close(client_sock);
+      close(client_sock); /*close the socket*/
       printf("Client disconnected");
       
   }
