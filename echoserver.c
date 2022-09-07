1271701dc1f66ed5d63d38a081d2908f80f8d9ca
gaosong628 <51213408+gaosong628@users.noreply.github.com>
2022-09-06 22:37:37 -0500

    Update echoserver.c

--- echoserver.c
+++ echoserver.c
@@ -11,20 +11,7 @@
 
 #define BUFSIZE 256
 
-#define USAGE                                                        \
-  "usage:\n"                                                         \
-  "  echoserver [options]\n"                                         \
-  "options:\n"                                                       \
-  "  -p                  Port (Default: 10823)\n"                    \
-  "  -m                  Maximum pending connections (default: 5)\n" \
-  "  -h                  Show this help message\n"
 
-/* OPTIONS DESCRIPTOR ====================================================== */
-static struct option gLongOptions[] = {
-    {"help", no_argument, NULL, 'h'},
-    {"port", required_argument, NULL, 'p'},
-    {"maxnpending", required_argument, NULL, 'm'},
-    {NULL, 0, NULL, 0}};
 
 int main(int argc, char **argv) {
   int maxnpending = 5;
@@ -35,39 +22,6 @@ int main(int argc, char **argv) {
   socklen_t addr_size;
   char buffer[BUFSIZE];
   int n;
-
-  // Parse and set command line arguments
-  while ((option_char =
-              getopt_long(argc, argv, "hx:m:p:", gLongOptions, NULL)) != -1) {
-    switch (option_char) {
-      case 'm':  // server
-        maxnpending = atoi(optarg);
-        break;
-      case 'p':  // listen-port
-        portno = atoi(optarg);
-        break;
-      case 'h':  // help
-        fprintf(stdout, "%s ", USAGE);
-        exit(0);
-        break;
-      default:
-        fprintf(stderr, "%s ", USAGE);
-        exit(1);
-    }
-  }
-
-  setbuf(stdout, NULL);  // disable buffering
-
-  if ((portno < 1025) || (portno > 65535)) {
-    fprintf(stderr, "%s @ %d: invalid port number (%d)\n", __FILE__, __LINE__,
-            portno);
-    exit(1);
-  }
-  if (maxnpending < 1) {
-    fprintf(stderr, "%s @ %d: invalid pending count (%d)\n", __FILE__, __LINE__,
-            maxnpending);
-    exit(1);
-  }
 
   /* Socket Code Here */
   /*create a socket and verify*/
@@ -88,18 +42,18 @@ int main(int argc, char **argv) {
     perror("[-]Bind error");
     exit(1);
   }
-  printf("[+]Bind to the port number: %d\n", portno);
+  //printf("[+]Bind to the port number: %d\n", portno);
   
   /*make sure server is ready to listen*/
   listen(server_sock, 5);
-  printf("Listening...\n");
+  //printf("Listening...\n");
   
   while(1)
   {
       /*accept data package*/
       addr_size=sizeof(client_addr);
       client_sock=accept(server_sock, (struct sockaddr*)&client_addr, &addr_size);
-      printf("[+]Client connected.\n");
+      //printf("[+]Client connected.\n");
       
     while(1) {
       bzero(buffer,BUFSIZE);
@@ -111,7 +65,7 @@ int main(int argc, char **argv) {
       exit(1);
      }
 
-      printf("Message Received from Client: %s\n",buffer);
+      //printf("Message Received from Client: %s\n",buffer);
       /*send the same message back to the client*/
       send(client_sock,buffer,strlen(buffer),0);
     }
