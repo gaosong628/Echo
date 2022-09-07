1535395dfc92372d469e2b9d757cbec17098f189
gaosong628 <51213408+gaosong628@users.noreply.github.com>
2022-09-06 22:34:53 -0500

    Update echoclient.c

--- echoclient.c
+++ echoclient.c
@@ -12,23 +12,6 @@
 // A buffer large enough to contain the longest allowed string 
 #define BUFSIZE 256
 
-#define USAGE                                                          \
-  "usage:\n"                                                           \
-  "  echoclient [options]\n"                                           \
-  "options:\n"                                                         \
-  "  -s                  Server (Default: localhost)\n"                \
-  "  -p                  Port (Default: 10823)\n"                      \
-  "  -m                  Message to send to server (Default: \"Hello " \
-  "Summer.\")\n"                                                       \
-  "  -h                  Show this help message\n"
-
-/* OPTIONS DESCRIPTOR ====================================================== */
-static struct option gLongOptions[] = {
-    {"server", required_argument, NULL, 's'},
-    {"port", required_argument, NULL, 'p'},
-    {"message", required_argument, NULL, 'm'},
-    {"help", no_argument, NULL, 'h'},
-    {NULL, 0, NULL, 0}};
 
 /* Main ========================================================= */
 int  main(int argc, char **argv) 
@@ -44,48 +27,6 @@ int  main(int argc, char **argv)
   long Hostaddress;
   int n=1;
  
-
-  // Parse and set command line arguments
-  while ((option_char =
-              getopt_long(argc, argv, "p:s:m:hx", gLongOptions, NULL)) != -1) {
-    switch (option_char) {
-      default:
-        fprintf(stderr, "%s", USAGE);
-        exit(1);
-      case 's':  // server
-        hostname = optarg;
-        break;
-      case 'p':  // listen-port
-        portno = atoi(optarg);
-        break;
-      case 'h':  // help
-        fprintf(stdout, "%s", USAGE);
-        exit(0);
-        break;
-      case 'm':  // message
-        message = optarg;
-        break;
-    }
-  }
-
-  setbuf(stdout, NULL);  // disable buffering
-
-  if ((portno < 1025) || (portno > 65535)) {
-    fprintf(stderr, "%s @ %d: invalid port number (%d)\n", __FILE__, __LINE__,
-            portno);
-    exit(1);
-  }
-
-  if (NULL == message) {
-    fprintf(stderr, "%s @ %d: invalid message\n", __FILE__, __LINE__);
-    exit(1);
-  }
-
-  if (NULL == hostname) {
-    fprintf(stderr, "%s @ %d: invalid host name\n", __FILE__, __LINE__);
-    exit(1);
-  }
-
   /* Socket Code Here */
   /*create a socket and verify it*/
   sock = socket(AF_INET, SOCK_STREAM, 0);
@@ -112,7 +53,7 @@ int  main(int argc, char **argv)
    bzero(buffer,BUFSIZE);
    if (n>1) 
    { 
-    printf("Please type in Message sending to Server: ");
+    //printf("Please type in Message sending to Server: ");
     fgets(buffer,sizeof(buffer),stdin);
      
      if (strlen(buffer)==1 || strlen(buffer)>16)
@@ -122,7 +63,7 @@ int  main(int argc, char **argv)
    }
    else
    {
-    printf("Message sending to Server For the First Time: \n"); 
+    //printf("Message sending to Server For the First Time: \n"); 
     strcpy(buffer,message);
    }
     n=n+1;
